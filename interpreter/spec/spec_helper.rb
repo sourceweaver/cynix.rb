f9c84d9b95597bc13fc57f8fec26d85ba5ad26ac
sourceweaver <sw.git@pm.me>
2022-10-28 00:39:37 +0300

    update coverage output

--- interpreter/spec/spec_helper.rb
+++ interpreter/spec/spec_helper.rb
@@ -1,9 +1,9 @@
-require_relative '../cynix'
-require_relative '../environment'
-
 require 'simplecov'
 require 'simplecov-lcov'
 
 SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
 SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 SimpleCov.start
+
+require_relative '../cynix'
+require_relative '../environment'
