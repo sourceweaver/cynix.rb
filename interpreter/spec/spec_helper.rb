dcb3cbe069f6035ea109511d1ce698027fa4ea46
sourceweaver <sw.git@pm.me>
2022-10-28 00:02:55 +0300

    integrate coveralls Signed-off-by: sourceweaver <sw.git@pm.me>

--- interpreter/spec/spec_helper.rb
+++ interpreter/spec/spec_helper.rb
@@ -1,2 +1,8 @@
 require_relative '../cynix'
 require_relative '../environment'
+
+require 'simplecov'
+SimpleCov.start
+
+require 'simplecov-cobertura'
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
